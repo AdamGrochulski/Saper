bf4e75f4d877eadd1b3e39451ade41f16b05cf7f
AdamGrochulski <adam.grochulskiii@gmail.com>
2025-01-22 17:13:41 +0100

    Mała poprawka i dodanie komentarzy do sweeper.c

--- sweeper.c
+++ sweeper.c
@@ -32,35 +32,42 @@ void commandPicker(Board *board, Pos *pos, int type) {
     
     if (type==1) {
             switch (choice) {
-                case 'r':
+                case 'r': //Odsłanianie pola
                     playerMove(board,pos,x-1,y-1,0);
                     break;
-                case 'f':
+                case 'f': //Stawianie flagi
                     place_flag(board,pos,x-1,y-1);
                     break;
+                default:
+                    printf("Niepoprawna komenda!\n");
+                    break;
+
             }
     }
-    else {
+    else { //Przypadek, gdy gracz się rusza po raz pierwszy (konieczny dla wygenerowania tablicy)
         if (choice=='r') {
             pos->x=x-1;
             pos->y=y-1;
         }
         else {
-            printf("Pierwsze polecenie musi być wybraniem pola!\n");
+            printf("Pierwsze polecenie musi być wybraniem pola!\n"); //Jeśli nie została wybrana komenda 'r' to wtedy prosimy jeszcze raz
             commandPicker(board, pos,0);
         }
     }
 }
+//Funkcja odsłaniająca pola po ruchu gracza
 void revealTiles(Board *board, int x, int y) {
     int i_around,j_around;
+    //Przejscie po polach wokół pola [x,y], jeśli jest puste
     if (board->data[x][y] == 0 && board->shown[x][y] == ' ') {
         board->shown[x][y]=to_char(board->data[x][y]);
         for(j_around=y-1;j_around<=y+1;j_around++) {
             if (j_around>=0 && j_around<board->c) {
                 for(i_around=x-1;i_around<=x+1;i_around++) {
                     if (i_around>=0 && i_around<board->r) {
+                        //Sprawdzanie czy pole wokół pola [x,y] nie jest miną
                         if (board->data[i_around][j_around] != -1) {
-                            revealTiles(board,i_around,j_around);
+                            revealTiles(board,i_around,j_around); //Rekurencyjne sprawdzanie następnych pól wokół [i_around,j_around]
                             board->shown[i_around][j_around] = to_char(board->data[i_around][j_around]);
                         }
                     }
@@ -68,11 +75,13 @@ void revealTiles(Board *board, int x, int y) {
             }
         }
     }
+    // Gdy pole jest już odslonięte to po prostu je jeszcze raz "pokaż" (zabieg zrobiony w celu uniknięcia błędów)
     else {
         board->shown[x][y]=to_char(board->data[x][y]);
     }
 }
 
+//Funckja ruchu gracza, sprawdza czy pole jest bombą, jeśli nie, to odkrywa następne pola. W przeciwnym przypadku gra się kończy
 void playerMove(Board *board, Pos *pos, int x, int y, int type) {
     pos->x=x;
     pos->y=y;
@@ -85,7 +94,7 @@ void playerMove(Board *board, Pos *pos, int x, int y, int type) {
     }
     printBoard(board);
 }       
-
+//Funckja pomocniczna która zamienia integer na string
 char* toString(int num) {
     int length = snprintf( NULL, 0, "%d", num );
     char* str = malloc( length + 1 );
