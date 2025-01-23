946dbccc08e99816d31cf638c0c5dff316fb1a58
AdamGrochulski <adam.grochulskiii@gmail.com>
2025-01-23 15:54:14 +0100

    Wprowadzenie getopt do main.c, poprawienie Makefile oraz dodanie opcji clean - do usuwania zbędnych plików wywołania

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
 all:
 	cc board.c highscore.c levels.c sweeper.c main.c -o game
-	./game
-	rm game
 debug:
-	cc -Wall -pedantic board.c highscore.c levels.c sweeper.c main.c -o debug_mode 
-	./debug_mode
-	rm debug_mode
+	cc -Wall -pedantic board.c highscore.c levels.c sweeper.c main.c -o game
+clean:
+	@[ -f ./game ] && rm game || true
 
 
 
--- main.c
+++ main.c
@@ -2,8 +2,39 @@
 #include "board.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
-void main() {
+int main(int argc, char** argv) {
+    int option;
     Colors(0); //Zamienie czcionki na bold na czas trwania całej gry.
-    gameEngine();
+
+    while(option = getopt(argc, argv, "pfh")) {
+        switch(option) {
+            case 'p':
+                printf("Play\n");
+                gameEngine();
+                break;
+            case 'f':
+                FILE *file = argc > 2 ? fopen(argv[2],"r") : NULL;
+                if(file == NULL) {
+                    printf("Błąd podczas otwierania pliku!\n");
+                    exit(0);
+                }
+                char buffer[100];
+                while(fgets(buffer, sizeof(buffer), file))
+                    printf("%s",buffer);
+                break;
+            case 'h':
+                printf("Pomoc:\n");
+                printf("-p -> uruchamia grę w trybie standardowym");
+                printf("-f -> uruchamia grę zapisaną w pliku");
+                printf("-h -> wyświetla instrukcje wywołania gry");
+                break;
+            default:
+                printf("Koniec działania programu!\n");
+                return 0;
+            
+        }
+    }
+    return 1;
 }
