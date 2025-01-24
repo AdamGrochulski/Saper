24e6c670df0c205a7256baac90d8bf82b03dea55
AdamGrochulski <adam.grochulskiii@gmail.com>
2025-01-24 10:34:59 +0100

    Last minute changes

--- sweeper.c
+++ sweeper.c
@@ -17,7 +17,7 @@ void gameEngine(FILE *file) {
         //Rozgrywka
         revealTiles(board,pos->x,pos->y);
         printBoard(board);
-        printBoardDebug(board);
+        //printBoardDebug(board);
         board->flag_remain = board->m;
         while (board->Run==0) {
             commandPicker(board, pos, 1);
